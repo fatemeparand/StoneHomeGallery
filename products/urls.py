45ebd4a07d0b0c93486ecf09fa0917316f3c79a9
fateme_parand <fateme.parand94@gmail.com>
2023-04-25 15:41:10 +0330

    Convert function view to class_base view

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
-    path('new product/', views.product_create, name='product_create'),
-    path('<int:pk>/update product/', views.product_update, name='product_update'),
-    path('<int:pk>/delete product/', views.product_delete, name='product_delete'),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('new product/', views.ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/update product/', views.ProductUpdate.as_view(), name='product_update'),
+    path('<int:pk>/delete product/', views.ProductDelete.as_view(), name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,55 +1,87 @@
-import requests
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 from .models import Product
 from .forms import ProductCreationForm
 
 
-def product_list(request):
-    products = Product.objects.filter(active=True).order_by('-datetime_modified')
+class ProductList(generic.ListView):
+    context_object_name = 'products'
+    template_name = 'product/product_list.html'
 
-    context = {'products': products}
-    return render(request, 'product/product_list.html', context)
-
-
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-
-    context = {'product': product}
-    return render(request, 'product/product_detail.html', context)
+    def get_queryset(self):
+        return Product.objects.filter(active=True).order_by('-datetime_modified')
 
 
-def product_create(request):
-    if request.method == 'POST':
-        form = ProductCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('products:product_list')
-
-    else:
-        form = ProductCreationForm()
-
-    context = {'form': form}
-    return render(request, 'product/product_create.html', context)
+class ProductDetail(generic.DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/product_detail.html'
 
 
-def product_update(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-
-    form = ProductCreationForm(request.POST or None, instance=product)
-    if form.is_valid():
-        form.save()
-        return redirect(product.get_absolute_url())
-
-    context = {'form': form}
-    return render(request, 'product/product_create.html', context)
+class ProductCreate(generic.CreateView):
+    model = Product
+    form_class = ProductCreationForm
+    context = 'form'
+    template_name = 'product/product_create.html'
 
 
-def product_delete(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+class ProductUpdate(generic.UpdateView):
+    model = Product
+    form_class = ProductCreationForm
+    context_object_name = 'form'
+    template_name = 'product/product_create.html'
 
-    if request.method == 'POST':
-        product.delete()
-        return redirect('products:product_list')
 
-    context = {'product': product}
-    return render(request, 'product/product_delete.html', context)
+class ProductDelete(generic.DeleteView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/product_delete.html'
+    success_url = reverse_lazy('products:product_list')
+
+
+# def product_list(request):
+#     products = Product.objects.filter(active=True).order_by('-datetime_modified')
+#
+#     context = {'products': products}
+#     return render(request, 'product/product_list.html', context)
+
+# def product_detail(request, pk):
+#     product = Product.objects.get(pk=pk)
+#
+#     context = {'product': product}
+#     return render(request, 'product/product_detail.html', context)
+
+# def product_create(request):
+#     if request.method == 'POST':
+#         form = ProductCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('products:product_list')
+#
+#     else:
+#         form = ProductCreationForm()
+#
+#     context = {'form': form}
+#     return render(request, 'product/product_create.html', context)
+
+# def product_update(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#
+#     form = ProductCreationForm(request.POST or None, instance=product)
+#     if form.is_valid():
+#         form.save()
+#         return redirect(product.get_absolute_url())
+#
+#     context = {'form': form}
+#     return render(request, 'product/product_create.html', context)
+
+# def product_delete(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#
+#     if request.method == 'POST':
+#         product.delete()
+#         return redirect('products:product_list')
+#
+#     context = {'product': product}
+#     return render(request, 'product/product_delete.html', context)
